016c8f8d7f0901b6f198ca78533d0ea647d746b1
Martin Halamíček <martin@halamicek.cz>
2015-11-19 17:48:36 +0100

    super token test

--- tests/CommonTest.php
+++ tests/CommonTest.php
@@ -8,10 +8,12 @@
 
 namespace Keboola\ManageApiTest;
 
+use Keboola\ManageApi\Client;
+
 class CommonTest extends ClientTestCase
 {
 
-    public function testVerifyToken()
+    public function testVerifyAdminToken()
     {
         $token = $this->client->verifyToken();
 
@@ -22,5 +24,23 @@ class CommonTest extends ClientTestCase
         $this->assertFalse($token['isExpired']);
         $this->assertInternalType('array', $token['scopes']);
         $this->assertEquals($token['type'], 'admin');
+    }
+
+    public function testVerifySuperToken()
+    {
+        $client = new Client([
+            'token' => getenv('KBC_SUPER_API_TOKEN'),
+            'url' => getenv('KBC_MANAGE_API_URL'),
+            'backoffMaxTries' => 1,
+        ]);
+        $token = $client->verifyToken();
+
+        $this->assertInternalType('int', $token['id']);
+        $this->assertNotEmpty($token['description']);
+        $this->assertNotEmpty($token['created']);
+        $this->assertFalse($token['isDisabled']);
+        $this->assertFalse($token['isExpired']);
+        $this->assertInternalType('array', $token['scopes']);
+        $this->assertEquals($token['type'], 'super');
     }
 }
