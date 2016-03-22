a87c7b8b2a747ddadcf3ea71777fe9ca293f100e
Erik Zigo <erik.zigo@keboola.com>
2016-03-22 21:16:06 +0100

    feat(projects): undelete

--- tests/ProjectsTest.php
+++ tests/ProjectsTest.php
@@ -680,7 +680,7 @@ class ProjectsTest extends ClientTestCase
         $this->client->deleteProject($project['id']);
 
         $projects = $this->client->listDeletedProjects($params);
-        $this->assertCount(0, $projects);
+        $this->assertCount(1, $projects);
 
         $this->client->deleteOrganization($organization['id']);
     }
