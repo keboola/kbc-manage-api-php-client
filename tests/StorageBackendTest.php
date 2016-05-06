544f156ded86238eae14d66e565e7434939500da
Martin Halamíček <martin@halamicek.cz>
2016-05-06 14:55:07 +0200

    tests - project backend assignment and initialization

--- tests/StorageBackendTest.php
+++ tests/StorageBackendTest.php
@@ -28,7 +28,19 @@ class StorageBackendTest extends ClientTestCase
 
     public function testStorageAssignBackend()
     {
-        $this->markTestSkipped('just for development');
+        // get redshift backend
+        $backends = $this->client->listStorageBackend();
+        $redshiftBackend = null;
+        foreach ($backends as $backend) {
+            if ($backend['backend'] == 'redshift') {
+                $redshiftBackend = $backend;
+                break;
+            }
+        }
+        if (!$redshiftBackend) {
+            $this->fail('Redshift backend not found');
+        }
+
         $name = 'My org';
         $organization = $this->client->createOrganization($this->testMaintainerId, [
             'name' => $name,
@@ -39,23 +51,40 @@ class StorageBackendTest extends ClientTestCase
         ]);
 
         $this->assertArrayHasKey('backends', $project);
+        $this->assertEquals('mysql', $project['defaultBackend']);
 
         $backends = $project['backends'];
 
         $this->assertArrayHasKey('mysql', $backends);
         $this->assertArrayNotHasKey('redshift', $backends);
         
-        $this->client->assignProjectStorageBackend($project['id'], 6);
+        
+        $this->client->assignProjectStorageBackend($project['id'], $redshiftBackend['id']);
 
         $project = $this->client->getProject($project['id']);
         $backends = $project['backends'];
 
         $this->assertArrayHasKey('redshift', $backends);
+        $this->assertEquals('redshift', $project['defaultBackend']);
 
         $redshift = $backends['redshift'];
-        $this->assertEquals(6, $redshift['id']);
+        $this->assertEquals($redshiftBackend['id'], $redshift['id']);
 
-        $this->client->assignProjectStorageBackend($project['id'], 4);
+        // let's try to create a bucket in project now
+        $token = $this->client->createProjectStorageToken($project['id'], [
+            'description' => 'test',
+            'expiresIn' => 60,
+            'canManageBuckets' => true,
+        ]);
+
+        $sapiClient = new \Keboola\StorageApi\Client([
+            'url' => getenv('KBC_MANAGE_API_URL'),
+            'token' => $token['token'],
+        ]);
+        $bucketId = $sapiClient->createBucket('test', 'in');
+        $bucket = $sapiClient->getBucket($bucketId);
+        $this->assertEquals('redshift', $bucket['backend']);
+
     }
 
     public function testStorageBackendList()
