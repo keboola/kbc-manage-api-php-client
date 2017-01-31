6c5f5e6acc2a54ea65669121455d86e58836c09d
pivnicek <marc@keboola.com>
2017-01-31 16:59:39 +0100

    test: updated maintainers tests

--- tests/MaintainersTest.php
+++ tests/MaintainersTest.php
@@ -8,11 +8,177 @@
 
 namespace Keboola\ManageApiTest;
 
+use Keboola\ManageApi\Client;
 use Keboola\ManageApi\ClientException;
 
 class MaintainersTest extends ClientTestCase
 {
+    
+    /**
+     * @var Client
+     */
+    private $normalUserClient;
 
+    private $normalUser;
+    
+    private $superAdmin;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->normalUserClient = new \Keboola\ManageApi\Client([
+            'token' => getenv('KBC_TEST_ADMIN_TOKEN'),
+            'url' => getenv('KBC_MANAGE_API_URL')
+        ]);
+        $this->normalUser = $this->normalUserClient->verifyToken()['user'];
+        $this->superAdmin = $this->client->verifyToken()['user'];
+    }
+
+    /**
+     * Clean up any created maintainers or users added
+     */
+    public function tearDown()
+    {
+        $maintainers = $this->client->listMaintainers();
+        foreach ($maintainers as $maintainer) {
+            if ($maintainer['id'] === $this->testMaintainerId) {
+                $members = $this->client->listMaintainerMembers($maintainer['id']);
+                foreach ($members as $member) {
+                    if ($member['id'] != $this->superAdmin['id']) {
+                        $this->client->removeUserFromMaintainer($maintainer['id'], $member['id']);
+                    }
+                }
+            } else {
+                $this->client->deleteMaintainer($maintainer['id']);
+            } 
+        }
+        parent::tearDown(); 
+    }
+
+    public function testCreateDeleteMaintainer()
+    {
+        $testMaintainer = $this->client->getMaintainer($this->testMaintainerId);
+
+        $newMaintainer = $this->client->createMaintainer([
+            'name' => "test maintainer",
+            'idDefaultConnectionMysql' => $testMaintainer['idDefaultConnectionMysql'],
+            'idDefaultConnectionRedshift' => $testMaintainer['idDefaultConnectionRedshift'],
+            'idDefaultConnectionSnowflake' => $testMaintainer['idDefaultConnectionSnowflake'],
+        ]);
+
+        $this->assertEquals('test maintainer', $newMaintainer['name']);
+        $this->assertArrayHasKey('created', $newMaintainer);
+        $this->assertEquals($testMaintainer['idDefaultConnectionMysql'], $newMaintainer['idDefaultConnectionMysql']);
+        $this->assertEquals($testMaintainer['idDefaultConnectionRedshift'], $newMaintainer['idDefaultConnectionRedshift']);
+        $this->assertEquals($testMaintainer['idDefaultConnectionSnowflake'], $newMaintainer['idDefaultConnectionSnowflake']);
+        $this->assertArrayHasKey('zendesk_dropboxId', $newMaintainer);
+        $this->assertArrayHasKey('zendesk_url', $newMaintainer);
+        $this->assertNull($newMaintainer['zendesk_dropboxId']);
+        $this->assertNull($newMaintainer['zendesk_url']);
+
+        $maintainerList = $this->client->listMaintainers();
+        $this->assertCount(2, $maintainerList);
+
+        $this->client->deleteMaintainer($newMaintainer['id']);
+
+        $maintainerList = $this->client->listMaintainers();
+        $this->assertCount(1, $maintainerList);
+
+        // retrieve the deleted maintainer should throw 404
+        try {
+            $deletedMaintainer = $this->client->getMaintainer($newMaintainer['id']);
+            $this->fail("retrieve the deleted maintainer should throw 404");
+        } catch (ClientException $e) {
+            $this->assertEquals(404, $e->getCode());
+        }
+    }
+
+    public function testUpdateMaintainer()
+    {
+        $backends = $this->client->listStorageBackend();
+        foreach ($backends as $backend) {
+            switch ($backend['backend']) {
+                case 'mysql':
+                    $mysqlBackend = $backend;
+                    break;
+                case 'redshift':
+                    $redshiftBackend = $backend;
+                    break;
+                case 'snowflake':
+                    $snowflakeBackend = $backend;
+            }
+        }
+
+        $newMaintainer = $this->client->createMaintainer([
+            'name' => "test maintainer"
+        ]);
+        $updateArray = ['name' => 'updated name'];
+        if (!is_null($mysqlBackend)) {
+            $updateArray['idDefaultConnectionMysql'] = $mysqlBackend['id'];
+        }
+        if (!is_null($redshiftBackend)) {
+            $updateArray['idDefaultConnectionRedshift'] = $redshiftBackend['id'];
+        }
+        if (!is_null($snowflakeBackend)) {
+            $updateArray['idDefaultConnectionSnowflake'] = $snowflakeBackend['id'];
+        }
+
+        $updateArray['zendesk_url'] = "https://fake.url.com";
+        $updateArray['zendesk_dropboxId'] = "12345";
+
+        $updatedMaintainer = $this->client->updateMaintainer($newMaintainer['id'], $updateArray);
+
+        $this->assertEquals('updated name', $updatedMaintainer['name']);
+        $this->assertEquals("https://fake.url.com", $updatedMaintainer['zendesk_url']);
+        $this->assertEquals("12345", $updatedMaintainer['zendesk_dropboxId']);
+
+        if (array_key_exists('idDefaultConnectionMysql',$updateArray)) {
+            $this->assertEquals($mysqlBackend['id'], $updatedMaintainer['idDefaultConnectionMysql']);
+        }
+        if (array_key_exists('idDefaultConnectionRedshift',$updateArray)) {
+            $this->assertEquals($redshiftBackend['id'], $updatedMaintainer['idDefaultConnectionRedshift']);
+        }
+        if (array_key_exists('idDefaultConnectionSnowflake',$updateArray)) {
+            $this->assertEquals($mysqlBackend['id'], $updatedMaintainer['idDefaultConnectionSnowflake']);
+        }
+    }
+
+    public function testNormalUserMaintainerPermissions()
+    {
+        $testMaintainer = $this->client->getMaintainer($this->testMaintainerId);
+        try {
+            $newMaintainer = $this->normalUserClient->createMaintainer([
+                'name' => "test maintainer",
+                'idDefaultConnectionMysql' => $testMaintainer['idDefaultConnectionMysql'],
+                'idDefaultConnectionRedshift' => $testMaintainer['idDefaultConnectionRedshift'],
+                'idDefaultConnectionSnowflake' => $testMaintainer['idDefaultConnectionSnowflake'],
+            ]);    
+            $this->fail("normal user should not be able to create maintainrer");
+        } catch (ClientException $e) {
+            $this->assertEquals(403, $e->getCode());
+        }
+        // normal user should have empty maintainer list
+        $maintainerList = $this->normalUserClient->listMaintainers();
+        $this->assertCount(0,$maintainerList);
+        try {
+            $this->normalUserClient->getMaintainer($this->testMaintainerId);
+            $this->fail("normal user cannot fetch maintainer which he is not a member of");
+        } catch (ClientException $e) {
+            $this->assertEquals(403, $e->getCode());
+        }
+
+        try {
+            $this->normalUserClient->deleteMaintainer($this->testMaintainerId);
+            $this->fail("normal user cannot delete a maintainer");
+        } catch (ClientException $e) {
+            $this->assertEquals(403, $e->getCode());
+        }
+        
+        $this->client->addUserToMaintainer($this->testMaintainerId, $this->normalUser['email']);
+        $testMaintainer = $this->normalUserClient->getMaintainer($this->testMaintainerId);
+        $this->assertNotEmpty($testMaintainer['name']);
+    }
+    
     public function testListMaintainers()
     {
         $maintainers = $this->client->listMaintainers();
@@ -24,6 +190,15 @@ class MaintainersTest extends ClientTestCase
         $this->assertInternalType('int', $maintainer['id']);
         $this->assertNotEmpty($maintainer['name']);
         $this->assertNotEmpty($maintainer['created']);
+        $this->assertArrayHasKey('idDefaultConnectionMysql', $maintainer);
+        $this->assertArrayHasKey('idDefaultConnectionRedshift', $maintainer);
+        $this->assertArrayHasKey('idDefaultConnectionSnowflake', $maintainer);
+        $this->assertArrayHasKey('zendesk_dropboxId', $maintainer);
+        $this->assertArrayHasKey('zendesk_url', $maintainer);
+        $this->assertNull($maintainer['zendesk_dropboxId']);
+        $this->assertNull($maintainer['zendesk_url']);
+        $this->assertFalse($maintainer['isDeleted']);
+        $this->assertNull($maintainer['deletedTime']);
     }
     
     public function testUserMaintainerUsers()
@@ -33,19 +208,19 @@ class MaintainersTest extends ClientTestCase
         $this->assertGreaterThan(0, count($maintainers));
 
         $superAdmin = $this->client->verifyToken()['user'];
-
         $maintainer = $maintainers[0];
 
         $maintainerMembers = $this->client->listMaintainerMembers($maintainer['id']);
-        $this->assertCount(0,$maintainerMembers);
-
-        $this->client->addUserToMaintainer($maintainer['id'],['email' => $superAdmin['email']]);
-        $maintainerMembers = $this->client->listMaintainerMembers($maintainer['id']);
         $this->assertCount(1,$maintainerMembers);
 
-        $this->client->removeUserFromMaintainer($maintainer['id'], $superAdmin['id']);
+        $this->client->addUserToMaintainer($maintainer['id'],['email' => $this->normalUser['email']]);
         $maintainerMembers = $this->client->listMaintainerMembers($maintainer['id']);
-        $this->assertCount(0,$maintainerMembers);
+        $this->assertCount(2,$maintainerMembers);
+
+        $this->client->removeUserFromMaintainer($maintainer['id'], $this->normalUser['id']);
+        $maintainerMembers = $this->client->listMaintainerMembers($maintainer['id']);
+        $this->assertCount(1,$maintainerMembers);
+        $this->assertEquals($superAdmin['email'], $maintainerMembers[0]['email']);
 
     }
 
@@ -56,22 +231,16 @@ class MaintainersTest extends ClientTestCase
     // 4) confirm invited maintainer is active
     public function testMaintainerNoJoinOrganization()
     {
-        $normalUserClient = new \Keboola\ManageApi\Client([
-            'token' => getenv('KBC_TEST_ADMIN_TOKEN'),
-            'url' => getenv('KBC_MANAGE_API_URL')
-        ]);
-        $normalUser = $normalUserClient->verifyToken()['user'];
-
         $organization = $this->client->createOrganization($this->testMaintainerId, [
             'name' => 'Test org',
         ]);
 
-        // make normalUser a maintainer
-        $this->client->addUserToMaintainer($this->testMaintainerId,['email' => $normalUser['email']]);
+        // make sure normalUser is a maintainer
+        $this->addNormalMaintainer();
 
         // make sure maintainer cannot join organization
         try {
-            $this->client->addUserToOrganization($organization['id'], ["email" => $normalUser['email']]);
+            $this->client->addUserToOrganization($organization['id'], ["email" => $this->normalUser['email']]);
             $this->fail("Cannot add maintainers to organization");
         } catch (ClientException $e) {
             $this->assertEquals("manage.joinOrganizationPermissionDenied", $e->getStringCode());
@@ -80,23 +249,16 @@ class MaintainersTest extends ClientTestCase
 
     public function testSettingAutoJoinFlag()
     {
-        $normalUserClient = new \Keboola\ManageApi\Client([
-            'token' => getenv('KBC_TEST_ADMIN_TOKEN'),
-            'url' => getenv('KBC_MANAGE_API_URL')
-        ]);
-        $normalUser = $normalUserClient->verifyToken()['user'];
-        $superAdmin = $this->client->verifyToken()['user'];
-
         $organization = $this->client->createOrganization($this->testMaintainerId, [
             'name' => 'Test org',
         ]);
 
-        // make normalUser a maintainer
-        $this->client->addUserToMaintainer($this->testMaintainerId,['email' => $normalUser['email']]);
+        // make sure normalUser is a maintainer
+        $this->addNormalMaintainer();
 
         // make sure superAdmin cannot update allowAutoJoin
         try {
-            $org = $normalUserClient->updateOrganization($organization['id'], ['allowAutoJoin' => false]);
+            $org = $this->normalUserClient->updateOrganization($organization['id'], ['allowAutoJoin' => false]);
             $this->fail("Maintainers not allowed to alter 'allowAutoJoin` parameter");
         } catch (ClientException $e) {
             $this->assertEquals("manage.updateOrganizationPermissionDenied", $e->getStringCode());
@@ -108,24 +270,12 @@ class MaintainersTest extends ClientTestCase
 
     public function testMaintainerAutoJoin()
     {
-        $normalUserClient = new \Keboola\ManageApi\Client([
-            'token' => getenv('KBC_TEST_ADMIN_TOKEN'),
-            'url' => getenv('KBC_MANAGE_API_URL')
-        ]);
-        $normalUser = $normalUserClient->verifyToken()['user'];
-        $superAdmin = $this->client->verifyToken()['user'];
-
         $organization = $this->client->createOrganization($this->testMaintainerId, [
             'name' => 'Test org',
         ]);
-        $this->client->addUserToOrganization($organization['id'], [
-            "email" => $normalUser['email']
-        ]);
-        $this->client->removeUserFromOrganization($organization['id'], $superAdmin['id']);
 
-        // make normalUser a maintainer
-        $this->client->addUserToMaintainer($this->testMaintainerId,['email' => $normalUser['email']]);
-
+        // make sure normalUser is a maintainer
+        $this->addNormalMaintainer($this->normalUser);
 
         $testProject = $this->client->createProject($organization['id'], [
             'name' => 'Test Project',
@@ -133,60 +283,53 @@ class MaintainersTest extends ClientTestCase
 
         // allowAutoJoin is true, so maintainer should be allowed to join this new project
         $this->client->addUserToProject($testProject['id'],[
-            "email" => $normalUser['email']
+            "email" => $this->normalUser['email']
         ]);
         $projUsers = $this->client->listProjectUsers($testProject['id']);
         $this->assertCount(2,$projUsers);
         foreach ($projUsers as $projUser) {
             $this->assertEquals("active", $projUser['status']);
-            if ($projUser['email'] === $normalUser['email']) {
-                $this->assertEquals($projUser['id'], $normalUser['id']);
+            if ($projUser['email'] === $this->normalUser['email']) {
+                $this->assertEquals($projUser['id'], $this->normalUser['id']);
                 $this->assertEquals("active", $projUser['status']);
             } else {
-                $this->assertEquals($projUser['email'], $normalUser['email']);
+                $this->assertEquals($projUser['email'], $this->normalUser['email']);
             }
         }
-        $this->client->removeUserFromProject($testProject['id'],$superAdmin['id']);
+        $this->client->removeUserFromProject($testProject['id'],$this->superAdmin['id']);
         $projUsers = $this->client->listProjectUsers($testProject['id']);
         $this->assertCount(1,$projUsers);
 
-        $normalUserClient->updateOrganization($organization['id'], ['allowAutoJoin' => false]);
+        $this->normalUserClient->updateOrganization($organization['id'], ['allowAutoJoin' => false]);
 
         // now superAdmin should have access pending when he tries to join the project
         $this->client->addUserToProject($testProject['id'], [
-            'email' => $superAdmin['email'],
+            'email' => $this->superAdmin['email'],
             'reason' => "testing",
             'expirationSeconds' => 8600
         ]);
         $projUsers = $this->client->listProjectUsers($testProject['id']);
         $this->assertCount(2,$projUsers);
         foreach ($projUsers as $projUser) {
-            if ($projUser['email'] === $superAdmin['email']) {
-                $this->assertEquals($projUser['id'], $normalUser['id']);
+            if ($projUser['email'] === $this->superAdmin['email']) {
+                $this->assertEquals($projUser['id'], $this->normalUser['id']);
                 $this->assertEquals("pending", $projUser['status']);
                 $this->assertEquals("testing", $projUser['reason']);
             } else {
                 $this->assertEquals("active", $projUser['status']);
-                $this->assertEquals($projUser['email'], $normalUser['email']);
+                $this->assertEquals($projUser['email'], $this->normalUser['email']);
             }
         }
     }
 
     public function testInviteMaintainer()
     {
-        $normalUserClient = new \Keboola\ManageApi\Client([
-            'token' => getenv('KBC_TEST_ADMIN_TOKEN'),
-            'url' => getenv('KBC_MANAGE_API_URL')
-        ]);
-        $normalUser = $normalUserClient->verifyToken()['user'];
-        $superAdmin = $this->client->verifyToken()['user'];
-
         $organization = $this->client->createOrganization($this->testMaintainerId, [
             'name' => 'Test org',
         ]);
 
-        // make normalUser a maintainer
-        $this->client->addUserToMaintainer($this->testMaintainerId,['email' => $normalUser['email']]);
+        // make sure normalUser is a maintainer
+        $this->addNormalMaintainer($this->normalUser);
 
         $testProject = $this->client->createProject($organization['id'], [
             'name' => 'Test Project',
@@ -196,19 +339,33 @@ class MaintainersTest extends ClientTestCase
         $this->assertEquals(false, $org['allowAutoJoin']);
 
         $this->client->addUserToProject($testProject['id'],[
-            "email" => $normalUser['email']
+            "email" => $this->normalUser['email']
         ]);
 
         $projUsers = $this->client->listProjectUsers($testProject['id']);
         $this->assertCount(2,$projUsers);
         foreach ($projUsers as $projUser) {
             $this->assertEquals("active", $projUser['status']);
-            if ($projUser['email'] === $normalUser['email']) {
-                $this->assertEquals($projUser['id'], $normalUser['id']);
+            if ($projUser['email'] === $this->normalUser['email']) {
+                $this->assertEquals($projUser['id'], $this->normalUser['id']);
                 $this->assertEquals("active", $projUser['status']);
             } else {
-                $this->assertEquals($projUser['email'], $normalUser['email']);
+                $this->assertEquals($projUser['email'], $this->normalUser['email']);
             }
+        }
+    }
+
+    private function addNormalMaintainer() {
+        // make sure normalUser a maintainer
+        $maintainers = $this->client->listMaintainerMembers($this->testMaintainerId);
+        $normalMaintainerExists = false;
+        foreach ($maintainers as $maintainer) {
+            if ($maintainer['email'] === $this->normalUser['email']) {
+                $normalMaintainerExists = true;
+            }
+        }
+        if (!$normalMaintainerExists) {
+            $this->client->addUserToMaintainer($this->testMaintainerId,['email' => $this->normalUser['email']]);
         }
     }
 }
