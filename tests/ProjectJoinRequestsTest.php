f08ce428559233851d3a2ebab9390f3ff13a4860
Erik Zigo <erik.zigo@keboola.com>
2018-10-23 16:33:00 +0200

    tests(join-requests): admins request access

--- tests/ProjectJoinRequestsTest.php
+++ tests/ProjectJoinRequestsTest.php
@@ -41,10 +41,232 @@ class ProjectJoinRequestsTest extends ClientTestCase
         $this->organization = $organization;
         $this->project = $project;
 
+        foreach ($this->client->listMyProjectJoinRequests() as $joinRequest) {
+            $this->client->deleteMyProjectJoinRequest($joinRequest['id']);
+        }
+
         foreach ($this->normalUserClient->listMyProjectJoinRequests() as $joinRequest) {
             $this->normalUserClient->deleteMyProjectJoinRequest($joinRequest['id']);
         }
     }
+
+    public function testSuperAdminRequestAccess(): void
+    {
+        $this->client->removeUserFromOrganization($this->organization['id'], $this->superAdmin['id']);
+        $this->client->removeUserFromMaintainer($this->testMaintainerId, $this->superAdmin['id']);
+
+        $this->normalUserClient->updateOrganization($this->organization['id'], [
+            "allowAutoJoin" => 0
+        ]);
+
+        $projectId = $this->createProjectWithOrganizationMember();
+
+        $projectUser = $this->findProjectUser($projectId, $this->superAdmin['email']);
+        $this->assertNull($projectUser);
+
+        $joinRequests = $this->client->listMyProjectJoinRequests();
+        $this->assertCount(0, $joinRequests);
+
+        $joinRequest = $this->client->requestAccessToProject($projectId);
+        $joinRequest = $this->client->getProjectJoinRequest($projectId, $joinRequest['id']);
+
+        $this->assertEquals('', $joinRequest['reason']);
+        $this->assertEmpty($joinRequest['expires']);
+        $this->assertEquals($this->superAdmin['id'], $joinRequest['user']['id']);
+        $this->assertEquals($this->superAdmin['email'], $joinRequest['user']['email']);
+
+        $joinRequests = $this->client->listProjectJoinRequests($projectId);
+        $this->assertCount(1, $joinRequests);
+
+        $this->assertEquals($joinRequest, reset($joinRequests));
+
+        try {
+            $this->client->approveProjectJoinRequest($projectId, $joinRequest['id']);
+            $this->fail('Approve join request should produce error');
+        } catch (ClientException $e) {
+            $this->assertEquals(403, $e->getCode());
+        }
+
+        try {
+            $this->client->rejectProjectJoinRequest($projectId, $joinRequest['id']);
+            $this->fail('Reject join request should produce error');
+        } catch (ClientException $e) {
+            $this->assertEquals(403, $e->getCode());
+        }
+
+        $joinRequests = $this->client->listProjectJoinRequests($projectId);
+        $this->assertCount(1, $joinRequests);
+
+        $this->assertEquals($joinRequest, reset($joinRequests));
+    }
+
+    public function testSuperAdminRequestAccessError(): void
+    {
+        $this->client->removeUserFromOrganization($this->organization['id'], $this->superAdmin['id']);
+        $this->client->removeUserFromMaintainer($this->testMaintainerId, $this->superAdmin['id']);
+
+        $projectId = $this->createProjectWithOrganizationMember();
+
+        $projectUser = $this->findProjectUser($projectId, $this->superAdmin['email']);
+        $this->assertNull($projectUser);
+
+        $joinRequests = $this->client->listMyProjectJoinRequests();
+        $this->assertCount(0, $joinRequests);
+
+        try {
+            $this->client->requestAccessToProject($projectId);
+            $this->fail('Request access should produce error');
+        } catch (ClientException $e) {
+            $this->assertEquals(400, $e->getCode());
+        }
+
+        $joinRequests = $this->client->listMyProjectJoinRequests();
+        $this->assertCount(0, $joinRequests);
+    }
+
+    public function testMaintainerAdminRequestAccess(): void
+    {
+        $this->client->removeUserFromOrganization($this->organization['id'], $this->normalUser['id']);
+        $this->client->addUserToMaintainer($this->testMaintainerId, ['email' => $this->normalUser['name']]);
+
+        $this->client->updateOrganization($this->organization['id'], [
+            "allowAutoJoin" => 0
+        ]);
+
+        $projectId = $this->createProjectWithSuperAdminMember();
+
+        $projectUser = $this->findProjectUser($projectId, $this->normalUser['email']);
+        $this->assertNull($projectUser);
+
+        $joinRequests = $this->normalUserClient->listMyProjectJoinRequests();
+        $this->assertCount(0, $joinRequests);
+
+        $joinRequest = $this->normalUserClient->requestAccessToProject($projectId);
+        $joinRequest = $this->normalUserClient->getProjectJoinRequest($projectId, $joinRequest['id']);
+
+        $this->assertEquals('', $joinRequest['reason']);
+        $this->assertEmpty($joinRequest['expires']);
+        $this->assertEquals($this->normalUser['id'], $joinRequest['user']['id']);
+        $this->assertEquals($this->normalUser['email'], $joinRequest['user']['email']);
+
+        $joinRequests = $this->normalUserClient->listProjectJoinRequests($projectId);
+        $this->assertCount(1, $joinRequests);
+
+        $this->assertEquals($joinRequest, reset($joinRequests));
+
+        try {
+            $this->normalUserClient->approveProjectJoinRequest($projectId, $joinRequest['id']);
+            $this->fail('Approve join request should produce error');
+        } catch (ClientException $e) {
+            $this->assertEquals(403, $e->getCode());
+        }
+
+        try {
+            $this->normalUserClient->rejectProjectJoinRequest($projectId, $joinRequest['id']);
+            $this->fail('Reject join request should produce error');
+        } catch (ClientException $e) {
+            $this->assertEquals(403, $e->getCode());
+        }
+
+        $joinRequests = $this->normalUserClient->listProjectJoinRequests($projectId);
+        $this->assertCount(1, $joinRequests);
+
+        $this->assertEquals($joinRequest, reset($joinRequests));
+    }
+
+    public function testMaintainerAdminRequestAccessError(): void
+    {
+        $this->client->removeUserFromOrganization($this->organization['id'], $this->normalUser['id']);
+        $this->client->addUserToMaintainer($this->testMaintainerId, ['email' => $this->normalUser['name']]);
+
+        $projectId = $this->createProjectWithSuperAdminMember();
+
+        $projectUser = $this->findProjectUser($projectId, $this->normalUser['email']);
+        $this->assertNull($projectUser);
+
+        $joinRequests = $this->normalUserClient->listMyProjectJoinRequests();
+        $this->assertCount(0, $joinRequests);
+
+        try {
+            $this->normalUserClient->requestAccessToProject($projectId);
+            $this->fail('Request access should produce error');
+        } catch (ClientException $e) {
+            $this->assertEquals(400, $e->getCode());
+        }
+
+        $joinRequests = $this->normalUserClient->listMyProjectJoinRequests();
+        $this->assertCount(0, $joinRequests);
+    }
+
+    public function testOrganizationAdminRequestAccessError(): void
+    {
+        $projectId = $this->createProjectWithSuperAdminMember();
+
+        $projectUser = $this->findProjectUser($projectId, $this->normalUser['email']);
+        $this->assertNull($projectUser);
+
+        $joinRequests = $this->normalUserClient->listMyProjectJoinRequests();
+        $this->assertCount(0, $joinRequests);
+
+        try {
+            $this->normalUserClient->requestAccessToProject($projectId);
+            $this->fail('Request access should produce error');
+        } catch (ClientException $e) {
+            $this->assertEquals(400, $e->getCode());
+        }
+
+        $joinRequests = $this->normalUserClient->listProjectJoinRequests($projectId);
+        $this->assertCount(0, $joinRequests);
+
+        // project without autojoin
+        $this->client->updateOrganization($this->organization['id'], [
+            "allowAutoJoin" => 0
+        ]);
+
+        try {
+            $this->normalUserClient->requestAccessToProject($projectId);
+            $this->fail('Request access should produce error');
+        } catch (ClientException $e) {
+            $this->assertEquals(400, $e->getCode());
+        }
+
+        $joinRequests = $this->normalUserClient->listProjectJoinRequests($projectId);
+        $this->assertCount(0, $joinRequests);
+    }
+
+    public function testAdminRequestAccessError(): void
+    {
+        $this->client->removeUserFromOrganization($this->organization['id'], $this->normalUser['id']);
+
+        $projectId = $this->createProjectWithSuperAdminMember();
+
+        $projectUser = $this->findProjectUser($projectId, $this->normalUser['email']);
+        $this->assertNull($projectUser);
+
+        $joinRequests = $this->normalUserClient->listMyProjectJoinRequests();
+        $this->assertCount(0, $joinRequests);
+
+        try {
+            $this->normalUserClient->requestAccessToProject($projectId);
+            $this->fail('Request access should produce error');
+        } catch (ClientException $e) {
+            $this->assertEquals(403, $e->getCode());
+        }
+
+        $joinRequests = $this->normalUserClient->listMyProjectJoinRequests();
+        $this->assertCount(0, $joinRequests);
+    }
+
+
+
+
+
+
+
+
+
+
+
 
     public function testRequestAccess()
     {
@@ -535,5 +757,36 @@ class ProjectJoinRequestsTest extends ClientTestCase
 
         $invitations = $this->normalUserClient->listMyProjectJoinRequests();
         $this->assertCount(0, $invitations);
+    }
+
+    private function findProjectUser(int $projectId, string $userEmail): ?array
+    {
+        $projectUsers = $this->client->listProjectUsers($projectId);
+
+        foreach ($projectUsers as $projectUser) {
+            if ($projectUser['email'] === $userEmail) {
+                return $projectUser;
+            }
+        }
+
+        return null;
+    }
+
+    private function createProjectWithOrganizationMember(): int
+    {
+        $project = $this->normalUserClient->createProject($this->organization['id'], [
+            'name' => 'My test',
+        ]);
+
+        return $project['id'];
+    }
+
+    private function createProjectWithSuperAdminMember(): int
+    {
+        $project = $this->client->createProject($this->organization['id'], [
+            'name' => 'My test',
+        ]);
+
+        return $project['id'];
     }
 }
