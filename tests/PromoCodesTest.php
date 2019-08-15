2c036f5c34b7637e9bbf7955a3e4fdb1004c8dee
Ondra Jodas <ondra.jodas@gmail.com>
2019-08-15 16:21:10 +0200

    tests for promo codes

--- tests/PromoCodesTest.php
+++ tests/PromoCodesTest.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: martinhalamicek
- * Date: 15/10/15
- * Time: 15:29
- */
 
 namespace Keboola\ManageApiTest;
 
@@ -13,17 +7,39 @@ use Keboola\ManageApi\ClientException;
 class PromoCodesTest extends ClientTestCase
 {
 
-    public function testListAndCreatePromoCodes()
+    private $organization;
+
+    public function setUp()
     {
-        $organization = $this->client->createOrganization($this->testMaintainerId, [
+        parent::setUp();
+
+        $this->client->addUserToMaintainer($this->testMaintainerId, ['email' => 'spam+spam@keboola.com']);
+
+        foreach ($this->client->listMaintainerMembers($this->testMaintainerId) as $member) {
+            if ($member['id'] === $this->normalUser['id']) {
+                $this->client->removeUserFromMaintainer($this->testMaintainerId, $member['id']);
+            }
+
+            if ($member['id'] === $this->superAdmin['id']) {
+                $this->client->removeUserFromMaintainer($this->testMaintainerId, $member['id']);
+            }
+        }
+
+        $this->organization = $this->client->createOrganization($this->testMaintainerId, [
             'name' => 'My org for promo codes',
         ]);
 
+        $this->client->addUserToOrganization($this->organization['id'], ['email' => 'spam@keboola.com']);
+        $this->client->removeUserFromOrganization($this->organization['id'], $this->superAdmin['id']);
+    }
+
+    public function testSuperAdminCanListAndCreatePromoCodes()
+    {
         $promoCodesBeforeCreate = $this->client->listPromoCodesRequest($this->testMaintainerId);
         $promoCode = $this->client->createPromoCodeRequest($this->testMaintainerId, [
             'code' => 'TEST-' . time(),
             'expirationDays' => rand(5, 20),
-            'organizationId' => $organization['id'],
+            'organizationId' => $this->organization['id'],
             'projectTemplateStringId' => 'poc15DaysGuideMode',
         ]);
         $promoCodesAfterCreate = $this->client->listPromoCodesRequest($this->testMaintainerId);
@@ -31,6 +47,44 @@ class PromoCodesTest extends ClientTestCase
         $this->assertEquals(count($promoCodesBeforeCreate) + 1, count($promoCodesAfterCreate));
 
         $this->assertEquals($promoCode, $promoCodesAfterCreate[0]);
+    }
+
+    public function testCannotCreateDuplicatePromoCode()
+    {
+        $promoCode = [
+            'code' => 'TEST-' . time(),
+            'expirationDays' => rand(5, 20),
+            'organizationId' => $this->organization['id'],
+            'projectTemplateStringId' => 'poc15DaysGuideMode',
+        ];
+        $addedPromoCode = $this->client->createPromoCodeRequest($this->testMaintainerId, $promoCode);
+
+        $this->assertEquals($promoCode['code'], $addedPromoCode['code']);
+
+        $this->expectException(ClientException::class);
+        $this->expectExceptionCode(400);
+        $this->client->createPromoCodeRequest($this->testMaintainerId, $promoCode);
+    }
+
+    public function testOrganizationAdminCannotListPromoCode()
+    {
+        $this->expectException(ClientException::class);
+        $this->expectExceptionCode(403);
+
+        $this->normalUserClient->listPromoCodesRequest($this->testMaintainerId);
+    }
+
+    public function testOrganizationAdminCannotCreatePromoCode()
+    {
+        $this->expectException(ClientException::class);
+        $this->expectExceptionCode(403);
+
+        $this->normalUserClient->createPromoCodeRequest($this->testMaintainerId, [
+            'code' => 'TEST-' . time(),
+            'expirationDays' => rand(5, 20),
+            'organizationId' => $this->organization['id'],
+            'projectTemplateStringId' => 'poc15DaysGuideMode',
+        ]);
     }
 
     public function testInvalidOrganization()
@@ -62,7 +116,7 @@ class PromoCodesTest extends ClientTestCase
             ]);
             $this->fail('Project template not found');
         } catch (ClientException $e) {
-            $this->assertEquals(400, $e->getCode());
+            $this->assertEquals(404, $e->getCode());
         }
     }
 }
