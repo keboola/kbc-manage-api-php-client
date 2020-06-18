09d65d156af3675b1338f3b8a80642b01fca3b14
Vladimír Kriška <vlado@keboola.com>
2020-06-18 08:24:21 +0200

    Add test for app creation and deletion

--- tests/UiAppsTest.php
+++ tests/UiAppsTest.php
@@ -6,6 +6,42 @@ use Keboola\ManageApi\Client;
 
 class UiAppsTest extends ClientTestCase
 {
+    public function testAppCreationAndDeletion()
+    {
+        $client = new Client([
+            'token' => getenv('KBC_MANAGE_API_SUPER_WITH_UI_MANAGE_SCOPE'),
+            'url' => getenv('KBC_MANAGE_API_URL'),
+            'backoffMaxTries' => 1,
+        ]);
+
+        $newAppName = 'Sample KBC Application';
+
+        $listOfAppsBeforeCreation = array_map(function ($app) {
+            return $app['name'];
+        }, $client->listUiApps());
+        sort($listOfAppsBeforeCreation);
+
+        $client->registerUiApp([
+            'manifestUrl' => 'https://keboola.github.io/kbc-ui-app-manifest-file/sample.json',
+            'activate' => true,
+        ]);
+
+        $listOfAppsAfterCreation = array_map(function ($app) {
+            return $app['name'];
+        }, $client->listUiApps());
+        sort($listOfAppsAfterCreation);
+
+        $client->deleteUiApp($newAppName);
+
+        $listOfAppsAfterDeletion = array_map(function ($app) {
+            return $app['name'];
+        }, $client->listUiApps());
+        sort($listOfAppsAfterDeletion);
+
+        $this->assertTrue(count($listOfAppsBeforeCreation) === (count($listOfAppsAfterCreation) - 1));
+        $this->assertTrue(array_search($newAppName, $listOfAppsAfterCreation) !== false);
+        $this->assertEquals($listOfAppsBeforeCreation, $listOfAppsAfterDeletion);
+    }
 
     public function testPublicList()
     {
