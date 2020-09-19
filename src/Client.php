ab804da0196d39027849d4ddacdf02a1bb59063c
Erik Zigo <erik.zigo@keboola.com>
2020-09-19 10:30:25 +0200

    Client::chargeProjectCredits

--- src/Client.php
+++ src/Client.php
@@ -417,6 +417,11 @@ class Client
         return $this->apiPost("/manage/projects/{$projectId}/tokens", $params);
     }
 
+    public function chargeProjectCredits($projectId, $params = [])
+    {
+        return $this->apiPost("/manage/projects/{$projectId}/credits", $params);
+    }
+
     public function assignProjectStorageBackend($projectId, $backendId)
     {
         return $this->apiPost("/manage/projects/{$projectId}/storage-backend", [
