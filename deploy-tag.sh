369f7602b45c48016d8bd3bfd22a24b4f2b25285
Erik Zigo <erik.zigo@keboola.com>
2021-08-20 16:59:44 +0200

    build travis tag

--- deploy-tag.sh
+++ deploy-tag.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 docker login -u="$QUAY_USERNAME" -p="$QUAY_PASSWORD" quay.io
-docker tag keboola/manage-api-tests quay.io/keboola/manage-api-tests:$TRAVIS_TAG
+docker tag keboola/manage-api-tests:release quay.io/keboola/manage-api-tests:$TRAVIS_TAG
 docker images
 docker push quay.io/keboola/manage-api-tests:$TRAVIS_TAG
