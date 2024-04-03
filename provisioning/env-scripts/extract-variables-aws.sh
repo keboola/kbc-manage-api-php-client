5dda828a2f2480989de559114547863bdd8cba85
Martin Jandl <martin.jandl@gmail.com>
2024-04-03 20:50:34 +0200

    Scripts to update aws and azure env vars

---
+++ provisioning/env-scripts/extract-variables-aws.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+
+cd "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+source ./functions.sh
+
+# output variables
+output_var 'TEST_S3_REGION' $(terraform_output 'TEST_S3_REGION')
+output_var 'TEST_S3_FILES_BUCKET' $(terraform_output 'TEST_S3_FILES_BUCKET')
+output_var 'TEST_S3_KEY' $(terraform_output 'TEST_S3_KEY')
+output_var 'TEST_S3_SECRET' $(terraform_output 'TEST_S3_SECRET')
+output_var 'TEST_S3_ROTATE_KEY' $(terraform_output 'TEST_S3_ROTATE_KEY')
+output_var 'TEST_S3_ROTATE_SECRET' $(terraform_output 'TEST_S3_ROTATE_SECRET')
